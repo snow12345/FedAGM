9ee1dd90076748b24e021d93fe0855bb9bd44052
ninigapa0 <ninigapa@snu.ac.kr>
2021-07-07 15:13:04 +0900

    Auto model training

---
+++ Activation_regularization/L2/do_baseline_dirichlet06.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import shlex
+
+#iid_common=['python3','main.py']
+iid_common=['nohup','python3','main.py', '--config', 'configs/cifar_Fedavg.yaml','--data', './data','--mode','dirichlet','--dirichlet_alpha','0.6','--batch_size','50',"--centralized_epochs",'0',"--global_epochs",'1000','--local_epochs','5','--epsilon','0.1','--momentum','0','--lr','0.1','--learning_rate_decay','1','--weight_decay','1e-3','--seed','0','--set','CIFAR10','--arch','CNN','--workers','0','--cuda_visible_device','1']
+###################################################3
+this_argv=iid_common
+this_alpha=0
+this_argv.append('--alpha')
+this_argv.append(str(this_alpha))
+this_argv.append('--additional_experiment_name')
+this_argv.append('alpha'+str(this_alpha)+'_zeromomentum')
+print(this_argv)
+subprocess.Popen(this_argv) 
+#####################################################
