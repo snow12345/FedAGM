fix memory leakage bug

Activation_regularization/L2/global_update_method/delta_aggregation_fedDyn.py
@@ -10,9 +10,10 @@ from torch import nn
 import copy
 import torch
 
-def GlobalUpdate(args,device,trainset,testloader,LocalUpdate):
+
+def GlobalUpdate(args, device, trainset, testloader, LocalUpdate):
     model = get_model(args)
-    model.to(device)
+    # model.to(device)
     wandb.watch(model)
     criterion = nn.CrossEntropyLoss().to(device)
     model.train()
@@ -38,15 +39,14 @@ def GlobalUpdate(args,device,trainset,testloader,LocalUpdate):
         local_g[key] = torch.zeros_like(local_g[key]).to('cpu')
     local_deltas = {net_i: copy.deepcopy(local_g) for net_i in range(args.num_of_clients)}
 
-
     for epoch in range(args.global_epochs):
-        num_of_data_clients=[]
-        local_K=[]
-        
+        num_of_data_clients = []
+        local_K = []
+
         local_weight = []
         local_loss = []
         local_delta = []
-        #print('global delta of linear weight', global_delta['linear.weight'])
+        # print('global delta of linear weight', global_delta['linear.weight'])
         global_weight = copy.deepcopy(model.state_dict())
         m = max(int(args.participation_rate * args.num_of_clients), 1)
         selected_user = np.random.choice(range(args.num_of_clients), m, replace=False)
@@ -54,51 +54,55 @@ def GlobalUpdate(args,device,trainset,testloader,LocalUpdate):
         for user in selected_user:
             num_of_data_clients.append(len(dataset[user]))
             local_setting = LocalUpdate(args=args, lr=this_lr, local_epoch=args.local_epochs, device=device,
-                                        batch_size=args.batch_size, dataset=trainset, idxs=dataset[user], alpha=this_alpha, local_deltas=local_deltas)
+                                        batch_size=args.batch_size, dataset=trainset, idxs=dataset[user],
+                                        alpha=this_alpha, local_deltas=local_deltas)
             weight, loss = local_setting.train(net=copy.deepcopy(model).to(device), user=user)
             local_K.append(local_setting.K)
-            #weight, loss = local_setting.train(net=copy.deepcopy(model).to(device))
+            # weight, loss = local_setting.train(net=copy.deepcopy(model).to(device))
             local_weight.append(copy.deepcopy(weight))
             local_loss.append(copy.deepcopy(loss))
             ## store local delta
             delta = {}
             for key in weight.keys():
-                delta[key] = weight[key] - global_weight[key]
+                delta[key] = weight[key].to('cpu') - global_weight[key]
             local_delta.append(delta)
-            
-        total_num_of_data_clients=sum(num_of_data_clients)
+
+        total_num_of_data_clients = sum(num_of_data_clients)
         FedAvg_weight = copy.deepcopy(local_weight[0])
         for key in FedAvg_weight.keys():
             for i in range(len(local_weight)):
-                if i==0:
-                    FedAvg_weight[key]*=num_of_data_clients[i]
-                else:                       
-                    FedAvg_weight[key] += local_weight[i][key]*num_of_data_clients[i]
+                if i == 0:
+                    FedAvg_weight[key] *= num_of_data_clients[i]
+                else:
+                    FedAvg_weight[key] += local_weight[i][key] * num_of_data_clients[i]
             FedAvg_weight[key] /= total_num_of_data_clients
 
-        K_mean=sum(local_K)/len(local_K)
+        # K_mean=sum(local_K)/len(local_K)
         for key in global_delta.keys():
             for i in range(len(local_delta)):
-                if i==0:
+                if i == 0:
                     global_delta[key] = local_delta[0][key]
                 else:
                     global_delta[key] += local_delta[i][key]
             global_delta[key] = global_delta[key] * (-1 * args.alpha / args.num_of_clients)
-            #global_delta[key] = global_delta[key] / float((-1 * len(local_delta)))
-            #global_lr = args.g_lr
-            #print('global_lr', global_lr)
+            # global_delta[key] = global_delta[key] / float((-1 * len(local_delta)))
+            # global_lr = args.g_lr
+            # print('global_lr', global_lr)
+
             global_h[key] = global_h[key] + global_delta[key]
-            global_weight[key] = FedAvg_weight[key] - global_h[key] / args.alpha
-            #print((FedAvg_weight[key] == global_weight[key]).all())
+            global_weight[key] = FedAvg_weight[key].to('cpu') - global_h[key] / args.alpha
+
+            # print((FedAvg_weight[key] == global_weight[key]).all())
+
         ## global weight update
         model.load_state_dict(global_weight)
         loss_avg = sum(local_loss) / len(local_loss)
-        print(' num_of_data_clients : ',num_of_data_clients)  
+        print(' num_of_data_clients : ', num_of_data_clients)
         print(' Average loss {:.3f}'.format(loss_avg))
         loss_train.append(loss_avg)
         if epoch % args.print_freq == 0:
             model.eval()
-            model = model.cuda()
+            model = model.to(device)
             correct = 0
             total = 0
             with torch.no_grad():
@@ -114,8 +118,9 @@ def GlobalUpdate(args,device,trainset,testloader,LocalUpdate):
             acc_train.append(100 * correct / float(total))
 
         model.train()
+        model = model.to('cpu')
 
-        wandb.log({args.mode + '_loss': loss_avg, args.mode + "_acc": acc_train[-1],'lr':this_lr})
+        wandb.log({args.mode + '_loss': loss_avg, args.mode + "_acc": acc_train[-1], 'lr': this_lr})
 
         this_lr *= args.learning_rate_decay
         if args.alpha_mul_epoch == True:
@@ -123,15 +128,16 @@ def GlobalUpdate(args,device,trainset,testloader,LocalUpdate):
         elif args.alpha_divide_epoch == True:
             this_alpha = args.alpha / (epoch + 1)
 
-
     print('loss_train')
     print(loss_train)
 
     print('acc_train')
     print(acc_train)
 
-
 # In[ ]:
+
+
+
 
 
 

Activation_regularization/L2/local_update_method/fedDyn.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from utils import DatasetSplit
 import torch
 from local_update_method.global_and_online_model import *
+from tqdm import *
 
 class LocalUpdate(object):
     def __init__(self, args, lr, local_epoch, device, batch_size, dataset=None, idxs=None, alpha=0.0, local_deltas=None):
@@ -28,7 +29,7 @@ class LocalUpdate(object):
         # train and update
         optimizer = optim.SGD(model.parameters(), lr=self.lr, momentum=self.args.momentum,weight_decay=self.args.weight_decay)
         epoch_loss = []
-        local_delta = self.local_deltas[user]
+        local_delta = copy.deepcopy(self.local_deltas[user])
         for iter in range(self.local_epoch):
             batch_loss = []
             for batch_idx, (images, labels) in enumerate(self.ldr_train):
@@ -47,19 +48,21 @@ class LocalUpdate(object):
                 lg_loss = 0
                 for n, p in net.named_parameters():
                     p = torch.flatten(p)
-                    local_grad = torch.flatten(local_delta[n]).to(self.device)
+                    local_d = local_delta[n].detach().clone().to(self.device)
+                    local_grad = torch.flatten(local_d)
                     lg_loss += (p * local_grad.detach()).sum()
-                    local_grad.to('cpu')
 
+                #loss = ce_loss - lg_loss + 0.5 * self.args.mu * reg_loss
                 loss = ce_loss - lg_loss + 0.5 * self.args.mu * reg_loss
                 loss.backward()
                 #torch.nn.utils.clip_grad_norm_(model.parameters(), self.args.gr_clipping_max_norm)
                 optimizer.step()
                 batch_loss.append(loss.item())
 
-            ## Update Local Delta
-            for n, p in net.named_parameters():
-                self.local_deltas[user][n] = (local_delta[n].cpu() - self.args.alpha * (p - fixed_params[n].detach()).cpu()).cpu()
             epoch_loss.append(sum(batch_loss)/len(batch_loss))
         #model.to('cpu')
+
+        ## Update Local Delta
+        for n, p in net.named_parameters():
+            self.local_deltas[user][n] = (local_delta[n] - self.args.alpha * (p - fixed_params[n]).detach().clone().to('cpu'))
         return net.state_dict(), sum(epoch_loss) / len(epoch_loss)