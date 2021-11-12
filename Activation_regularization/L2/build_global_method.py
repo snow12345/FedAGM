82c8d2ef7d33f2f538720b494d44fa5240e05083
snow12345 <snow1234@korea.ac.kr>
2021-11-12 16:49:15 +0900

    add backtracking

--- Activation_regularization/L2/build_global_method.py
+++ Activation_regularization/L2/build_global_method.py
@@ -44,6 +44,8 @@ def build_global_update_module(args):
 
     elif args.global_method == 'FedNAG':
         from global_update_method.delta_aggregation_NAG import GlobalUpdate
+    elif args.global_method == 'FedNAG_backtracking':
+        from global_update_method.delta_aggregation_NAG_backtracking import GlobalUpdate
     elif args.global_method == 'FedNAG_dyn':
         from global_update_method.delta_aggregation_NAG_dyn import GlobalUpdate
     elif args.global_method == 'FedNAG_ctr_oversh':
